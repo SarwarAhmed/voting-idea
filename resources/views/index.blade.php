5ebd19bd3449af51f04d2ddafd85f92335b5b56c
Sarwar Ahmed <sarwar.amas@gmail.com>
2021-04-10 18:00:12 +0600

    Design Idea Cards

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -25,4 +25,171 @@
             </div>
         </div>
     </div> <!-- End filters -->
+
+    <div class="ideas-container space-y-6 my-6">
+        <div class="idea-container bg-white rounded-xl flex hover:shadow-card transition duration-150 ease-in cursor-pointer">
+            <div class="border-r border-gray-200 px-5 py-8">
+                <div class="text-center">
+                    <div class="font-semibold text-2xl">12</div>
+                    <div class="text-gray-500">Votes</div>
+                </div>
+
+                <div class="mt-8">
+                    <button class="w-20 bg-gray-200 border border-gray-200 hover:border-gray-400 font-bold uppercase text-xxs rounded-xl transition duration-150 ease-in px-4 py-3">Vote</button>
+                </div>
+            </div>
+            <div class="flex px-2 py-6">
+                <a href="#" class="flex-none">
+                    <img src="https://source.unsplash.com/200x200/?face&crop=face&v=1" alt="avatar" class="w-14 h-14 rounded-xl">
+                </a>
+                <div class="mx-4">
+                    <h4 class="text-xl font-semibold">
+                        <a href="#" class="hover:underline">Random title can go here...</a>
+                    </h4>
+                    <div class="text-gray-600 mt-3 line-clamp-3">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint nobis mollitia minus illum praesentium ab atque quidem eius enim? Iure mollitia tempora quam maiores consequuntur, qui asperiores
+                        
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint nobis mollitia minus illum praesentium ab atque quidem eius enim? Iure mollitia tempora quam maiores consequuntur, qui asperiores
+                    </div>
+
+                    <div class="flex items-center justify-between mt-6">
+                        <div class="flex items-center text-xs text-gray-400 font-semibold space-x-2">
+                            <div>10 Hours ago</div>
+                            <div>&bull;</div>
+                            <div>Category 1</div>
+                            <div>&bull;</div>
+                            <div class="text-gray-900">3 Comments</div>
+                        </div>
+
+                        <div class="flex items-center space-x-2">
+                            <div class="bg-gray-200 text-xxs font-bold uppercase leading-none rounded-full text-center w-28 h-7 py-2 px-4">Open</div>
+                            <button class="relative bg-gray-200 hover:bg-gray-300 rounded-full h-7 transition duration-150 ease-in py-2 px-3">
+                                <svg class="h-4" fill="currentColor" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z" />
+                                </svg>
+                                <ul class="absolute ml-8 w-44 font-semibold text-left bg-white shadow-dialog rounded-xl py-3">
+                                    <li>
+                                        <a href="#" class="hover:bg-gray-200 block transition duration-150 ease-in px-5 py-3">Mark as Span</a>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="hover:bg-gray-200 block transition duration-150 ease-in px-5 py-3">Delete Post</a>
+                                    </li>
+                                </ul>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div> <!-- End idea container -->
+        
+        <div class="idea-container bg-white rounded-xl flex hover:shadow-card transition duration-150 ease-in cursor-pointer">
+            <div class="border-r border-gray-200 px-5 py-8">
+                <div class="text-center">
+                    <div class="font-semibold text-2xl">12</div>
+                    <div class="text-gray-500">Votes</div>
+                </div>
+
+                <div class="mt-8">
+                    <button class="w-20 bg-blue text-white border border-blue hover:bg-blue-hover font-bold text-xxs uppercase rounded-xl transition duration-150 ease-in px-4 py-3">Voted</button>
+                </div>
+            </div>
+            <div class="flex px-2 py-6">
+                <a href="#" class="flex-none">
+                    <img src="https://source.unsplash.com/200x200/?face&crop=face&v=2" alt="avatar" class="w-14 h-14 rounded-xl">
+                </a>
+                <div class="mx-4">
+                    <h4 class="text-xl font-semibold">
+                        <a href="#" class="hover:underline">Another title can go here...</a>
+                    </h4>
+                    <div class="text-gray-600 mt-3 line-clamp-3">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint nobis mollitia minus illum praesentium ab atque quidem eius enim? Iure mollitia tempora quam maiores consequuntur, qui asperiores
+                        
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint nobis mollitia minus illum praesentium ab atque quidem eius enim? Iure mollitia tempora quam maiores consequuntur, qui asperiores
+                    </div>
+
+                    <div class="flex items-center justify-between mt-6">
+                        <div class="flex items-center text-xs text-gray-400 font-semibold space-x-2">
+                            <div>10 Hours ago</div>
+                            <div>&bull;</div>
+                            <div>Category 1</div>
+                            <div>&bull;</div>
+                            <div class="text-gray-900">3 Comments</div>
+                        </div>
+
+                        <div class="flex items-center space-x-2">
+                            <div class="bg-yellow text-white text-xxs font-bold uppercase leading-none rounded-full text-center w-28 h-7 py-2 px-4">In Progress</div>
+                            <button class="relative bg-gray-200 hover:bg-gray-300 rounded-full h-7 transition duration-150 ease-in py-2 px-3">
+                                <svg class="h-4" fill="currentColor" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z" />
+                                </svg>
+                                <ul class="hidden absolute ml-8 w-44 font-semibold text-left bg-white shadow-dialog rounded-xl py-3">
+                                    <li>
+                                        <a href="#" class="hover:bg-gray-200 block transition duration-150 ease-in px-5 py-3">Mark as Span</a>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="hover:bg-gray-200 block transition duration-150 ease-in px-5 py-3">Delete Post</a>
+                                    </li>
+                                </ul>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div> <!-- End idea container -->
+
+        <div class="idea-container bg-white rounded-xl flex hover:shadow-card transition duration-150 ease-in cursor-pointer">
+            <div class="border-r border-gray-200 px-5 py-8">
+                <div class="text-center">
+                    <div class="font-semibold text-2xl">12</div>
+                    <div class="text-gray-500">Votes</div>
+                </div>
+
+                <div class="mt-8">
+                    <button class="w-20 bg-gray-200 border border-gray-200 hover:border-gray-400 font-bold uppercase text-xxs rounded-xl transition duration-150 ease-in px-4 py-3">Vote</button>
+                </div>
+            </div>
+            <div class="flex px-2 py-6">
+                <a href="#" class="flex-none">
+                    <img src="https://source.unsplash.com/200x200/?face&crop=face&v=3" alt="avatar" class="w-14 h-14 rounded-xl">
+                </a>
+                <div class="mx-4">
+                    <h4 class="text-xl font-semibold">
+                        <a href="#" class="hover:underline">Yet another title can go here...</a>
+                    </h4>
+                    <div class="text-gray-600 mt-3 line-clamp-3">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint nobis mollitia minus illum praesentium ab atque quidem eius enim? Iure mollitia tempora quam maiores consequuntur, qui asperiores
+                        
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint nobis mollitia minus illum praesentium ab atque quidem eius enim? Iure mollitia tempora quam maiores consequuntur, qui asperiores
+                    </div>
+
+                    <div class="flex items-center justify-between mt-6">
+                        <div class="flex items-center text-xs text-gray-400 font-semibold space-x-2">
+                            <div>10 Hours ago</div>
+                            <div>&bull;</div>
+                            <div>Category 1</div>
+                            <div>&bull;</div>
+                            <div class="text-gray-900">3 Comments</div>
+                        </div>
+
+                        <div class="flex items-center space-x-2">
+                            <div class="bg-gray-200 text-xxs font-bold uppercase leading-none rounded-full text-center w-28 h-7 py-2 px-4">Open</div>
+                            <button class="relative bg-gray-200 hover:bg-gray-300 rounded-full h-7 transition duration-150 ease-in py-2 px-3">
+                                <svg class="h-4" fill="currentColor" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z" />
+                                </svg>
+                                <ul class="hidden absolute ml-8 w-44 font-semibold text-left bg-white shadow-dialog rounded-xl py-3">
+                                    <li>
+                                        <a href="#" class="hover:bg-gray-200 block transition duration-150 ease-in px-5 py-3">Mark as Span</a>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="hover:bg-gray-200 block transition duration-150 ease-in px-5 py-3">Delete Post</a>
+                                    </li>
+                                </ul>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div> <!-- End idea container --
+    </div> <!-- End ideas container -->
 </x-app-layout>
